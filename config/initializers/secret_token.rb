a293679245f2a3fedd1ff06aaa9bb4fa9f3a74fa
Junichi Ito <jit@sonicgarden.jp>
2013-05-30 03:10:58 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioSmsVerification::Application.config.secret_token = '059261946ad0c7b5d9e3c264debc69b422336cfa0875f105177f9a1ee2398448ef3657f7483ea6c2cffc3a8c0dc00110185c8d62337d3152141ad5cda4d964aa'
