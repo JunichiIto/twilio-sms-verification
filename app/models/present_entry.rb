0fd55e92cbc764dcae4146755e91ce8e40443af3
Junichi Ito <jit@sonicgarden.jp>
2013-05-30 08:43:15 +0900

    Format mobile phone number to send verification code

--- app/models/present_entry.rb
+++ app/models/present_entry.rb
@@ -4,6 +4,7 @@ class PresentEntry < ActiveRecord::Base
   
   attr_accessible :address, :email, :mobile_phone_number, :name
   validates_presence_of :address, :email, :mobile_phone_number, :name
+  validates :mobile_phone_number, format: { with: /^\d{3}-?\d{4}-?\d{4}$/ }, allow_blank: true
 
   after_create :issue_verification_code
   after_create :send_verification_code
@@ -18,12 +19,16 @@ class PresentEntry < ActiveRecord::Base
   def send_verification_code
     twilio_client.account.sms.messages.create(
       from: ENV["TWILIO_NUMBER"],
-      to: self.mobile_phone_number,
+      to: formatted_mobile_phone_number,
       body: "この認証コードを入力してください。\n#{verification_code}"
     )
   end
 
   def twilio_client
     @twilio_client ||= Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
+  end
+
+  def formatted_mobile_phone_number
+    "+81#{self.mobile_phone_number.gsub('-', '')[1..-1]}"
   end
 end
--- spec/models/present_entry_spec.rb
+++ spec/models/present_entry_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe PresentEntry do
+  let(:messages) { stub 'messages' }
+  before do
+    messages.stub(:create)
+    Twilio::REST::Client.stub_chain(:new, :account, :sms, :messages).and_return(messages)
+  end
+
   describe '#new' do
     it 'is not verified by default' do
       expect(build :present_entry).not_to be_verified
@@ -8,13 +14,7 @@ describe PresentEntry do
   end
 
   describe '#save' do
-    let(:messages) { stub 'messages' }
     let(:present_entry) { build :present_entry }
-
-    before do
-      messages.stub(:create)
-      Twilio::REST::Client.stub_chain(:new, :account, :sms, :messages).and_return(messages)
-    end
 
     it 'has random verification code after create' do
       expect{present_entry.save}.to change{present_entry.verification_code}.from(nil)
@@ -23,6 +23,47 @@ describe PresentEntry do
     it 'sends verification code after create' do
       messages.should_receive(:create)
       present_entry.save
+    end
+
+    it 'formats mobile phone number' do
+      messages.should_receive(:create).with(hash_including(to: "+818000000000"))
+      present_entry.save
+    end
+  end
+
+  describe 'validation' do
+    describe 'for mobile_phone_number' do
+      let(:present_entry) { build :present_entry, mobile_phone_number: mobile_phone_number }
+      context 'when contain hypens' do
+        let(:mobile_phone_number) { '080-1234-1234' }
+        specify do
+          expect{present_entry.valid?}.not_to change{present_entry.errors[:mobile_phone_number]}.from([])
+        end
+      end
+      context 'when contain no hypens' do
+        let(:mobile_phone_number) { '08012341234' }
+        specify do
+          expect{present_entry.valid?}.not_to change{present_entry.errors[:mobile_phone_number]}.from([])
+        end
+      end
+      context 'when contain non number' do
+        let(:mobile_phone_number) { '080-1234-123A' }
+        specify do
+          expect{present_entry.valid?}.to change{present_entry.errors[:mobile_phone_number]}.from([])
+        end
+      end
+      context 'when too short' do
+        let(:mobile_phone_number) { '0801234123' }
+        specify do
+          expect{present_entry.valid?}.to change{present_entry.errors[:mobile_phone_number]}.from([])
+        end
+      end
+      context 'when too long' do
+        let(:mobile_phone_number) { '080-1234-12345' }
+        specify do
+          expect{present_entry.valid?}.to change{present_entry.errors[:mobile_phone_number]}.from([])
+        end
+      end
     end
   end
 end
